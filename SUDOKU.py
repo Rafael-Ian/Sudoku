54de94f32c7b04d96982ca8f2ef8e82ae3ff4cba
Rafael-Ian <cabertesilorio@gmai.com>
2025-02-27 01:24:20 +0800

    board2

--- SUDOKU.py
+++ SUDOKU.py
@@ -8,12 +8,23 @@ def create_board():
         ["F", "", "", "A", "I", "E", "", "", ""],
         ["", "I", "H", "", "", "", "", "F", ""],
         ["H", "", "", "", "F", "", "", "", "C"],
-        ["D", ]
-        
-        
+        ["D", "", "", "H", "", "C", "", "", "A"],
+        ["G", "", "", "", "B", "", "", "", "F"],
+        ["", "F", "", "", "", "", "B", "H", ""],
+        ["", "", "", "D", "A", "I", "", "", "E"],
+        ["", "", "", "", "H", "", "", "G", "I"],
     ]
+
 #Check if letter placement is valid within Sudoku Rules
+def is_valid(board, row, col, letter):
 #Check row and column for duplicate letters
+    for i in range (9):
+        if (board[row][i] == letter and i != col) or (board[i][col] == letter and i != row):
+            return False 
+#Check 3x3 box for duplicate letters
+    box_row, box_col = (row // 3) * 3, (col // 3) * 3
+    for i in range (3)
+    
 #Check for incorrect letter replacement
 #Background color white for Sudoku Board
 #Draw Grid Lines
