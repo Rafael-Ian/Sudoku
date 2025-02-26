ff1c16cc8d5317d6ff09319f328efae85c0f220f
Rafael-Ian <cabertesilorio@gmai.com>
2025-02-27 01:37:27 +0800

    Board info

--- SUDOKU.py
+++ SUDOKU.py
@@ -23,12 +23,27 @@ def is_valid(board, row, col, letter):
             return False 
 #Check 3x3 box for duplicate letters
     box_row, box_col = (row // 3) * 3, (col // 3) * 3
-    for i in range (3)
-    
+    for i in range (3):
+        for j in range (3):
+            r, c = box_row + i, box_col + j
+            if board[r][c] == letter and (r,c) != (row, col):
+                return False
+    return True
+
+
 #Check for incorrect letter replacement
+#Sudoku board with highlighting and error indicators
+def draw_board(win, board, selected, incorrect_cells, fixed_cells):
 #Background color white for Sudoku Board
+    win.fill((255, 255, 255))
 #Draw Grid Lines
+    for i range(10):
+        pygame.draw.line(win, (0, 0, 0,), (50 * i, 0), (50 * i, 450), 2)
+        pygame.draw.line(win, (0, 0, 0,), (0, 50 * i), (450, 50 * i), 2)
+
 #Draw letters on the board
+
+
 #Colors: Pre-filled letters(Black), Incorrect Placement(Red), Player Input(Blue)
 #Highlight selected cell
 #Game Loop
