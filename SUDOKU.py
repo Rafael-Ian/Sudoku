0425594fc0d24eb5659b102206fa20650ccb751d
Rafael-Ian <cabertesilorio@gmai.com>
2025-02-27 01:11:59 +0800

    Board1

--- SUDOKU.py
+++ SUDOKU.py
@@ -1,4 +1,17 @@
+import pygame
+import sys
+
 #Create a pre-built Sudoku Board with letters ranging from A-I
+def create_board():
+    return [
+        ["E", "C", "", "", "G", "", "", "", ""],
+        ["F", "", "", "A", "I", "E", "", "", ""],
+        ["", "I", "H", "", "", "", "", "F", ""],
+        ["H", "", "", "", "F", "", "", "", "C"],
+        ["D", ]
+        
+        
+    ]
 #Check if letter placement is valid within Sudoku Rules
 #Check row and column for duplicate letters
 #Check for incorrect letter replacement
