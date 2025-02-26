67c058a4bd2ba76ba1883d76674395e1daf444c7
Rafael-Ian <cabertesilorio@gmai.com>
2025-02-27 00:54:07 +0800

    Add1

--- SUDOKU.py
+++ SUDOKU.py
@@ -1,2 +1,5 @@
 #Create a pre-built Sudoku Board with letters ranging from A-I
+#Check if letter placement is valid within Sudoku Rules
+#Check row and column for duplicate letters
+#Check for incorrect letter replacement
 #
