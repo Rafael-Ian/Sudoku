e2fcd008a2888e3990a2e889b79c91c51c5f21c4
Rafael-Ian <cabertesilorio@gmai.com>
2025-02-27 01:07:52 +0800

    Board

--- SUDOKU.py
+++ SUDOKU.py
@@ -2,4 +2,14 @@
 #Check if letter placement is valid within Sudoku Rules
 #Check row and column for duplicate letters
 #Check for incorrect letter replacement
+#Background color white for Sudoku Board
+#Draw Grid Lines
+#Draw letters on the board
+#Colors: Pre-filled letters(Black), Incorrect Placement(Red), Player Input(Blue)
+#Highlight selected cell
+#Game Loop
+#Check mistakes
+#Arrow Key Game Navigation
+#Select cell using mouse also
+#Check if the board is correctly completed
 #
